864149b8b0b5d98fd1cc104d9e4d2f8843a09773
nou-ros <nouros44@gmail.com>
2021-11-18 16:22:07 +0600

    mastering component added

---
+++ Modern React with Redux/4_mastering_class_component/src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import  React from 'react';
+
+class SearchBar extends React.Component{
+
+    state = { term: '' };
+    
+    onInputChange = (event) => {
+        // console.log(event.target.value);
+        this.setState({ term: event.target.value })
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        //Todo : make sure we call callback from parent component
+        this.props.onTermSearch(this.state.term);
+    }
+
+    render(){
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input 
+                        type='text' 
+                        onChange={this.onInputChange}
+                        value={this.state.term} 
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
